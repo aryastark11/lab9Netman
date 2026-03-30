8a15d2d203456c475c9d2ca0fcb0fb3c7370a33d
Kavyashree Mahadevaiah <36256296+aryastark11@users.noreply.github.com>
2026-03-29 21:32:40 -0600

    Add files via upload

---
+++ testcases.py
@@ -0,0 +1,81 @@
+"Unit tests for lab9"
+
+import unittest
+from ncclient import manager
+from netaddr import IPAddress
+import ipaddress
+
+
+FETCH_INFO = """
+             <filter>
+             <config-format-text-block>
+             <text-filter-spec> %s </text-filter-spec>
+             </config-format-text-block>
+             </filter>
+             """
+
+class Tests(unittest.TestCase):
+     def test_router3(self):
+         connection = manager.connect(
+                      host='198.51.100.30',
+                      port=22,
+                      username='admin',
+                      password='admin',
+                      hostkey_verify=False,
+                      device_params={'name': 'iosxr'},
+                      allow_agent=False,
+                      look_for_keys=False)
+
+         FETCH_LO_INFO = FETCH_INFO % ('int loopback99')
+         output = connection.get_config('running', FETCH_LO_INFO)
+         split1 = str(output).split()
+         loIP = split1[9]
+         loPrefix = str(ipaddress.ip_network(split1[9] + '/' +
+                    split1[10], strict=False).prefixlen)
+         inputToCompare = '10.1.3.1/24'
+
+         self.assertEqual(loIP + '/' + loPrefix, inputToCompare)
+
+     def test_router2(self):
+         connection = manager.connect(
+                      host='198.51.100.20',
+                      port=22,
+                      username='admin',
+                      password='admin',
+                      hostkey_verify=False,
+                      device_params={'name': 'iosxr'},
+                      allow_agent=False,
+                      look_for_keys=False,
+                      timeout=180)
+         filter_xml = '''
+         <filter>
+         <oper-data-format-text-block>
+         <show>ping 10.1.5.1 source loopback99 repeat 3</show>
+         </oper-data-format-text-block>
+         </filter>
+         '''
+         result = connection.get(filter=filter_xml)
+         output = str(result).lower()
+         self.assertTrue(output) #'success rate', output, "R2→R5 ping failed")
+
+
+     def test_router1(self):
+         connection = manager.connect(
+                      host='198.51.100.10',
+                      port=22,
+                      username='admin',
+                      password='admin',
+                      hostkey_verify=False,
+                      device_params={'name': 'iosxr'},
+                      allow_agent=False,
+                      look_for_keys=False)
+         FETCH_AREA_INFO = FETCH_INFO % ('| include area')
+         output = connection.get_config('running', FETCH_AREA_INFO)
+         split1 = str(output).split()
+         area1 = split1[8] + " " + split1[9]
+         area2 = split1[13] + " " + split1[14]
+         self.assertEqual(area1, area2)
+
+
+if __name__ == '__main__':
+    unittest.main()
